563ad8b91955825fc52b87cedcef41b17d48ec91
lavanya <lavanyakaarthik@gmail.com>
2020-07-17 12:30:38 -0500

    Added changes

--- src/qaautomation/reversestring/ReverseString.java
+++ src/qaautomation/reversestring/ReverseString.java
@@ -3,6 +3,13 @@ package qaautomation.reversestring;
 public class ReverseString {
 
 	public static void main(String[] args) {
+		String str = "Selenium";
+		String rev="";
+		for (int i=str.length()-1;i>=0;i--)
+		{
+			rev = rev + str.charAt(i);
+		}
+		System.out.println(rev);
 		
 	}
 
